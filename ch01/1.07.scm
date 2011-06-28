26fcfefc4c0ca751c2a94a9a11d38566e3a29719
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2011-06-28 16:19:55 +0300

    exercise 1.7

---
+++ ch01/1.07.scm
@@ -0,0 +1,65 @@
+;; Exercise 1.7.
+;;
+;; The good-enough? test used in computing square roots will not be
+;; very effective for finding the square roots of very small numbers.
+;; Also, in real computers, arithmetic operations are almost always
+;; performed with limited precision. This makes our test inadequate 
+;; for very large numbers. Explain these statements, with examples 
+;; showing how the test fails for small and large numbers. An alternative
+;; strategy for implementing good-enough? is to watch how guess changes
+;; from one iteration to the next and to stop when the change is a very
+;; small fraction of the guess. Design a square-root procedure that uses
+;; this kind of end test. Does this work better for small and large numbers?
+
+(define (square x) (* x x))
+
+;; old version
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+    guess
+    (sqrt-iter (improve guess x)
+               x)))
+
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
+
+;; test
+(sqrt 9)
+(sqrt (+ 100 37))
+(sqrt (+ (sqrt 2) (sqrt 3)))
+(square (sqrt 1000))
+
+;; new version
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (good-enough? old-guess guess)
+  (< (abs (- old-guess guess)) 0.001))
+
+(define (sqrt-iter old-guess guess x)
+  (if (good-enough? old-guess guess)
+    guess
+    (sqrt-iter guess
+               (improve guess x)
+               x)))
+
+(define (sqrt x)
+  (sqrt-iter 0.0 1.0 x))
+
+;; test
+(sqrt 9)
+(sqrt (+ 100 37))
+(sqrt (+ (sqrt 2) (sqrt 3)))
+(square (sqrt 1000))
